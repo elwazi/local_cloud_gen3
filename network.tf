240b66687d73fe18d7c2ef0e7f8ddfceaa64c977
Dane Kennedy <kennedy.dane@gmail.com>
2023-02-05 12:11:24 +0200

    Fix network and firewalls

--- network.tf
+++ network.tf
@@ -85,6 +85,31 @@ resource "openstack_networking_secgroup_rule_v2" "ssh" {
   security_group_id = openstack_networking_secgroup_v2.gen3_ssh.id
 }
 
+resource "openstack_networking_secgroup_v2" "gen3_kubernetes" {
+  name        = "${var.name_prefix}-kubernetes"
+  description = "To access gen3 ssh"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "internal_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "192.168.10.0/24"
+  security_group_id = openstack_networking_secgroup_v2.gen3_kubernetes.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "kubernetes_api" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "192.168.10.0/24"
+  security_group_id = openstack_networking_secgroup_v2.gen3_kubernetes.id
+}
+
 resource "openstack_networking_floatingip_v2" "k8s_float_ip" {
   pool = "${var.floating_ip_pool_name}"
 }
