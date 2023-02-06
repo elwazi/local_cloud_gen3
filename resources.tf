3f163d55c9d7b8bf778f50f5e75269cae58bbb8f
Dane Kennedy <kennedy.dane@gmail.com>
2023-02-06 13:28:32 +0200

    Create group_vars/all local file for ansible

--- resources.tf
+++ resources.tf
@@ -28,3 +28,33 @@ resource "local_file" "hosts_cfg" {
   )
   filename = "inventory.ini"
 }
+
+resource "local_file" "group_vars_all" {
+  content = templatefile("templates/group_vars.all.tpl",
+    {
+      timezone = var.timezone
+      google_client_id = var.google_client_id
+      google_client_secret = var.google_client_secret
+#      gen3_subnet = var.gen3_subnet
+      gen3_hostname = var.gen3_hostname
+      k8s_node_float_ip = openstack_networking_floatingip_v2.k8s_float_ip.address
+      k8s_nodes = [for node in openstack_compute_instance_v2.k8s_nodes.*: node ]
+      k8s_control_plane = openstack_compute_instance_v2.k8s_control_plane
+      database_node = openstack_compute_instance_v2.database_node
+      admin_user = var.admin_user
+      postgres_user = var.postgres_user
+      postgres_password = var.postgres_password
+      postgres_fence_user = var.postgres_fence_user
+      postgres_fence_password = var.postgres_fence_password
+      postgres_peregrine_user = var.postgres_peregrine_user
+      postgres_peregrine_password = var.postgres_peregrine_password
+      postgres_sheepdog_user = var.postgres_sheepdog_user
+      postgres_sheepdog_password = var.postgres_sheepdog_password
+      postgres_indexd_user = var.postgres_indexd_user
+      postgres_indexd_password = var.postgres_indexd_password
+      postgres_arborist_user = var.postgres_arborist_user
+      postgres_arborist_password = var.postgres_arborist_password
+    }
+  )
+  filename = "group_vars/all"
+}
